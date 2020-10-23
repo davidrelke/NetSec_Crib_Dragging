227116073d8399c966c053822c3d7278e5130f11
David Nicolas Relke <david.relke@stud.tu-darmstadt.de>
2020-10-23 16:57:05 +0200

    xor the ciphertexts

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+
 ciphertext_1_file = open("challenge1.txt", "rb")
 ciphertext_2_file = open("challenge2.txt", "rb")
 
@@ -25,3 +30,10 @@ while byte2:
 print(f"Ciphertext 2 contains {len(ciphertext_2_bytes)} bytes:")
 for index, b in enumerate(ciphertext_2_bytes):
     print(f"{index}: {b}")
+
+ciphertext_xor = []
+for i in range(len(ciphertext_2_bytes)):
+    ciphertext_xor.append(byte_xor(ciphertext_1_bytes[i], ciphertext_2_bytes[i]))
+
+print(f"xored contains {len(ciphertext_xor)} bytes:")
+[print(f"{i}:{d}") for i, d in enumerate(ciphertext_xor)]
